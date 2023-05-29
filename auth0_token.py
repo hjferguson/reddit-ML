87f2add35b80004973f4055aadc4fb3233185266
Harlan Ferguson <harlanjazz@gmail.com>
2023-05-28 22:37:01 -0400

    can produce a df and save to csv

--- auth0_token.py
+++ auth0_token.py
@@ -1,36 +1,52 @@
 #in order to access the API, we need to get an Auth0 token that expires every 2 hours. 
 import requests
+import pandas as pd
+import time
 
 def show_sub_stats(secret_key, personal_key,user,password,subreddit):
     
+    df = pd.DataFrame(columns=['Subreddit', 'Title'])
 
-        auth = requests.auth.HTTPBasicAuth(personal_key,secret_key)
+    auth = requests.auth.HTTPBasicAuth(personal_key,secret_key)
 
-        data = {
+    data = {
         'grant_type' : 'password',
         'username' : user,
         'password' : password
-        }
+    }
 
-        headers = {'User-Agent' : 'predicts subreddit based off title of post'}
+    headers = {'User-Agent' : 'predicts subreddit based off title of post'}
 
-        res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
 
-        if res.status_code == 200:
-                TOKEN = res.json()['access_token']
-                headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+    if res.status_code == 200:
+        TOKEN = res.json()['access_token']
+        headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
 
-                res = requests.get("https://oauth.reddit.com/r/" + subreddit + "/hot", headers=headers)
+        cursor = None
+        for _ in range(20):  # make 20 requests, which will give up to 2000 posts
+            url = "https://oauth.reddit.com/r/" + subreddit + "/top?t=all"
+            if cursor is not None:
+                url += "?after=" + cursor
+            res = requests.get(url, headers=headers)
 
-                if res.status_code == 200:  # successful request
-                        response_json = res.json()
-                        if 'data' in response_json:
-                                for post in response_json['data']['children']:
-                                        print(post['data']['title'])
-                        else:
-                                print("The key 'data' is not in the response.")
-                                print("Response:", response_json)
+            if res.status_code == 200:  # successful request
+                response_json = res.json()
+                if 'data' in response_json:
+                    for post in response_json['data']['children']:
+                        new_row = {'Subreddit' : subreddit, 'Title' : post['data']['title']}
+                        df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
+                    cursor = response_json['data']['after']  # get the cursor for the next page of results
+                    if cursor is None:  # if there's no cursor, we've reached the end of the posts
+                        break
                 else:
-                        print("Failed to get data from Reddit API. Status code:", res.status_code)
-        else:
-                print("Failed to get token from Reddit API. Status code:", res.status_code)
+                    print("The key 'data' is not in the response.")
+                    print("Response:", response_json)
+            else:
+                print("Failed to get data from Reddit API. Status code:", res.status_code)
+            time.sleep(1)  # sleep for 1 second to respect the rate limit
+    else:
+        print("Failed to get token from Reddit API. Status code:", res.status_code)
+
+    return df
+
