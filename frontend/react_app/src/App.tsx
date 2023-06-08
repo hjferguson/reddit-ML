840823f513c8afa43f7455c2d54a1d07b2d231e7
Harlan Ferguson <harlanjazz@gmail.com>
2023-06-07 23:01:55 -0400

    added basic tsx frontend

--- frontend/react_app/src/App.tsx
+++ frontend/react_app/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [prediction, setPrediction] = useState(""); // add this line
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/predict/', { title });
+      console.log(response.data);
+      setPrediction(response.data.prediction); // add this line
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <p>Prediction: {prediction}</p> {/* add this line */}
     </div>
   );
 }
